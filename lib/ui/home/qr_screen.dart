5baa6925984417de0e6586a749443b425c2d35a0
ammaar12 <muhammadfarroos12@gmail.com>
2025-03-04 16:19:41 +0700

    Implement QR scanner functionality with barcode handling and navigation to order details

--- lib/ui/home/qr_screen.dart
+++ lib/ui/home/qr_screen.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:ticketing_apps/core/assets/assets.gen.dart';
+import 'package:ticketing_apps/core/extensions/build_context_ext.dart';
+import 'package:ticketing_apps/ui/home/model/product_model.dart';
+import 'package:ticketing_apps/ui/home/order_detail_screen.dart';
 
-class QrScreen extends StatelessWidget {
+class QrScreen extends StatefulWidget {
   const QrScreen({super.key});
+
+  @override
+  State<QrScreen> createState() => _QrScreenState();
+}
+
+class _QrScreenState extends State<QrScreen> {
+  Barcode? _barcode;
+
+  void _handledBarcode(BarcodeCapture barcodes) {
+    if (mounted) {
+      _barcode = barcodes.barcodes.firstOrNull;
+      if (_barcode != null) {
+        debugPrint('Scan result -> ${{_barcode!.displayValue}}');
+        context.pushReplacement(OrderDetailScreen(products: dummyProducts));
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Ini QR screen page')),
+      appBar: AppBar(
+        title: Text('QR Scanner'),
+        leading: GestureDetector(
+          onTap: () => context.pop(),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Assets.images.back.image(),
+          ),
+        ),
+      ),
+      body: MobileScanner(onDetect: _handledBarcode),
     );
   }
 }
