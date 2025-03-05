972168e1fe71b1480d2fbeb2a46fb86a860fadf9
ammaar12 <muhammadfarroos12@gmail.com>
2025-03-05 13:03:01 +0700

    Add login request and response models with JSON serialization

---
+++ lib/model/response/login_response_model.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+
+class LoginResponseModel {
+    final String? token;
+    final User? user;
+
+    LoginResponseModel({
+        this.token,
+        this.user,
+    });
+
+    factory LoginResponseModel.fromJson(String str) => LoginResponseModel.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory LoginResponseModel.fromMap(Map<String, dynamic> json) => LoginResponseModel(
+        token: json["token"],
+        user: json["user"] == null ? null : User.fromMap(json["user"]),
+    );
+
+    Map<String, dynamic> toMap() => {
+        "token": token,
+        "user": user?.toMap(),
+    };
+}
+
+class User {
+    final int? id;
+    final String? name;
+    final String? email;
+    final dynamic phone;
+    final String? role;
+    final dynamic emailVerifiedAt;
+    final DateTime? createdAt;
+    final DateTime? updatedAt;
+
+    User({
+        this.id,
+        this.name,
+        this.email,
+        this.phone,
+        this.role,
+        this.emailVerifiedAt,
+        this.createdAt,
+        this.updatedAt,
+    });
+
+    factory User.fromJson(String str) => User.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory User.fromMap(Map<String, dynamic> json) => User(
+        id: json["id"],
+        name: json["name"],
+        email: json["email"],
+        phone: json["phone"],
+        role: json["role"],
+        emailVerifiedAt: json["email_verified_at"],
+        createdAt: json["created_at"] == null ? null : DateTime.parse(json["created_at"]),
+        updatedAt: json["updated_at"] == null ? null : DateTime.parse(json["updated_at"]),
+    );
+
+    Map<String, dynamic> toMap() => {
+        "id": id,
+        "name": name,
+        "email": email,
+        "phone": phone,
+        "role": role,
+        "email_verified_at": emailVerifiedAt,
+        "created_at": createdAt?.toIso8601String(),
+        "updated_at": updatedAt?.toIso8601String(),
+    };
+}
