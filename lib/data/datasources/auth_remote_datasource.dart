733d3d561f6b7459253cd39e52ec41bf31800ae0
ammaar12 <muhammadfarroos12@gmail.com>
2025-03-05 13:34:25 +0700

    Add authentication remote datasource with login functionality

---
+++ lib/data/datasources/auth_remote_datasource.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:ticketing_apps/core/constants/variables.dart';
+import 'package:ticketing_apps/model/request/login_request_model.dart';
+import 'package:ticketing_apps/model/response/login_response_model.dart';
+import 'package:http/http.dart' as http;
+
+class AuthRemoteDatasource {
+  Future<Either<String, LoginResponseModel>> login(LoginRequestModel data) async {
+    final response = await http.post(Uri.parse('${Variables.baseUrl}/api/login'),
+    headers: <String,String> {
+      'Content-Type' : 'application/json; charset=UTF-8',
+      'Accept' : 'application/json'
+    },
+    body: data.toJson()
+    );
+
+    if (response.statusCode == 200) {
+      return Right(LoginResponseModel.fromJson(response.body));
+    } else {
+      return Left(response.body);
+    }
+  }
+}
